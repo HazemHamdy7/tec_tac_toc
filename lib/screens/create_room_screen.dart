290e1495db115db9d5503e4683a4a720808a1489
HazemHamdy7 <hazemhamdy1215@gmail.com>
2023-06-02 09:37:58 +0300

    create room screen

--- lib/screens/create_room_screen.dart
+++ lib/screens/create_room_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tec_tac/responsive/resopnsive.dart';
+import 'package:tec_tac/widgets/custom_button.dart';
+import 'package:tec_tac/widgets/custom_text_filed.dart';
 
 import '../widgets/custom_text.dart';
 
@@ -12,26 +15,37 @@ class CreateRoomScreen extends StatefulWidget {
 }
 
 class _CreateRoomScreenState extends State<CreateRoomScreen> {
+  final TextEditingController _nameController = TextEditingController();
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const CustomText(
-              shadow: [Shadow(blurRadius: 90, color: Colors.blue)],
-              text: 'Create Room',
-              fontWeight: FontWeight.bold,
-              fontSize: 70,
-            ),
-            SizedBox(
-              height: size.height * 0.08,
-            )
-          ],
+      body: Responsive(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CustomText(
+                shadow: [Shadow(blurRadius: 40, color: Colors.blue)],
+                text: 'Create Room',
+                fontWeight: FontWeight.bold,
+                fontSize: 70,
+              ),
+              SizedBox(height: size.height * 0.08),
+              CustomTextField(
+                  controller: _nameController, hintText: 'Enter your name'),
+              SizedBox(height: size.height * 0.045),
+              CustomButton(onTap: () {}, text: 'Create')
+            ],
+          ),
         ),
       ),
     );
